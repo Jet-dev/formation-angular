b96547fb94858f427d76207d216422455c41e9ce
Liam <will.leemans@gmail.com>
2019-11-15 20:15:09 +0100

    :sparkles: : Lab 1 - setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { JetsComponent } from './jets/jets.component';
 
 import { SquadronEditComponent } from './squadron/squadron-edit/squadron-edit.component';
 import { SquadronComponent } from './squadron/squadron.component';
-import { JetItemComponent } from './jets/jet-list/jet-item/jet-item.component';
-import { JetListComponent } from './jets/jet-list/jet-list.component';
-import { JetDetailsComponent } from './jets/jet-details/jet-details.component';
+import { HeaderComponent } from './header/header.component';
+import { JetsComponent } from './jets/jets.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    JetListComponent,
-    JetItemComponent,
-    JetDetailsComponent,
     SquadronComponent,
     SquadronEditComponent,
     JetsComponent
