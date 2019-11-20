0c2698cebc65e759b4a779526942bd1a00c16fd5
Liam <will.leemans@gmail.com>
2019-11-20 22:24:30 +0100

    :sparkles: :  Add forms base

---
+++ src/app/pilot/pilot.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-pilot',
+  templateUrl: './pilot.component.html',
+  styleUrls: ['./pilot.component.css']
+})
+export class PilotComponent implements OnInit {
+
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
