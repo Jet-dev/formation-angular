246b7ec08427ec845aa12826f14f3e76c684edb9
Liam <will.leemans@gmail.com>
2019-11-20 22:24:30 +0100

    :sparkles: :  Add forms base

--- src/app/pilot/pilot.component.ts
+++ src/app/pilot/pilot.component.ts
@@ -8,11 +8,42 @@ import { NgForm } from '@angular/forms';
 })
 export class PilotComponent implements OnInit {
 
+  @ViewChild('f', { static: true }) pilotForm: NgForm;
+
+  flightPreferences = ['high', 'very high', 'to the moon'];
 
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  initForm() {
+    // this.pilotForm.form.setValue({
+    //   'contactInformation': {
+    //     firstName: 'Pete',
+    //     lastName: 'Mitchell',
+    //     email: 'maverick@jetdev.fr'
+    //   },
+    //   'preferedJet': 'Rafale',
+    //   'whyJet': 'It is cool !',
+    //   'checkMe': true,
+    //   'flightPreference': 'to the moon'
+    // })
+    this.pilotForm.form.patchValue({
+      'contactInformation': {
+        'firstName': 'Tom'
+      }
+    })
+  }
+
+  // classic
+  // onSubmit(pilotForm: NgForm) {
+  //   console.log(pilotForm);
+  // }
+
+  onSubmit() {
+    console.log(this.pilotForm);
   }
 
 }
